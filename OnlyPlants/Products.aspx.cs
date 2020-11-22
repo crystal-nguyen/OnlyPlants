3ffed9efe69b83d6cf0041c8cf953a1c9335beb6
Jill-Kapchan <jill.ingrid@gmail.com>
2020-11-22 03:15:04 -0700

    created the products.aspx page and deleted the html file

---
+++ OnlyPlants/Products.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlyPlants
+{
+    public partial class Products : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void OnAddToCart(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+
+            if(clickedButton.ID == "btnPlant1")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPlant2")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPlant3")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPlant4")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPlant5")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPlant6")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPlant7")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPlant8")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnSeeds1")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnSeeds2")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnSeeds3")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnSeeds4")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnSeeds5")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnSeeds6")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnSeeds7")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnSeeds8")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPots1")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPots2")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPots3")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPots4")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPots5")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPots6")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPots7")
+            {
+                
+            }
+            else if(clickedButton.ID == "btnPots8")
+            {
+                
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
