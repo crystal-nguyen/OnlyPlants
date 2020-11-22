b19eae8632a5ce2b052460e09fefeffdd731a7b6
Crystal Nguyen <cnguye30@sundevils.asu.edu>
2020-11-22 04:02:14 -0700

    added sql connection, test if existing user in sign up

--- OnlyPlants/Signup.aspx.cs
+++ OnlyPlants/Signup.aspx.cs
@@ -11,25 +11,41 @@ namespace OnlyPlants
 {
     public partial class Signup : System.Web.UI.Page
     {
+        private string connectionString = "Server=127.0.0.1;Port=5432;User Id=postgres;Password=0204999503cN;Database=postgres";
         protected void Page_Load(object sender, EventArgs e)
         {
-            string connectionString = "Server=127.0.0.1;Port=5432;User Id=postgres;Password=0204999503cN;Database=postgres";
-            using (var con = new NpgsqlConnection(connectionString))
-            {
-                con.Open();
-                var sql = "";
-                using (var cmd = new NpgsqlCommand(sql, con))
-                {
-                    var version = cmd.ExecuteScalar().ToString();
-                    f_name_tb.Text = version;
-                }
-            }
 
         }
 
         protected void signUp_Click(object sender, EventArgs e)
         {
+            string result = "";
+            using (var con = new NpgsqlConnection(connectionString))
+            {
+                con.Open();
+                var sql = @"SELECT userid FROM users WHERE login = @username";
+                using (var cmd = new NpgsqlCommand(sql, con))
+                {
+                    cmd.Parameters.AddWithValue("username", username_tb.Text);
 
+                    using(NpgsqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            result += dr.GetInt32(0);
+                        }
+                    }
+
+                }
+                con.Close();
+
+            }
+            if(result != "")
+            {
+                
+            }
+
+            
         }
     }
 }
